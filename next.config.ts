ce888f670028d2342865bc0b21ee789aefb3d537
MCA Things <mcastuff007@gmail.com>
2025-09-03 13:51:57 +0000

    ⚠ Cross origin request detected from 9003-firebase-studio-1754396360591.

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: ["9003-firebase-studio-1754396360591.cluster-a6zx3cwnb5hnuwbgyxmofxpkfe.cloudworkstations.dev"],
+  },
 };
 
 export default nextConfig;
